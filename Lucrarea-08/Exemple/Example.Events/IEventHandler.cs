bfa33838c27d5859cdd7d2b7dc11f3e36e47c1f2
Daniel Iercan <Daniel.Iercan@theaccessgroup.com>
2024-09-21 18:01:00 +0300

    actualizare lucreare 8

---
+++ Lucrarea-08/Exemple/Example.Events/IEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using CloudNative.CloudEvents;
+using Example.Events.Models;
+
+namespace Example.Events
+{
+    public interface IEventHandler
+    {
+        string[] EventTypes { get; }
+
+        Task<EventProcessingResult> HandleAsync(CloudEvent cloudEvent);
+    }
+}
